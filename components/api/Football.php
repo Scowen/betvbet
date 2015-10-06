4cd641654a9cef9e01e276a759e9192f2659dfe2
Xylum <lmscowen@gmail.com>
2015-10-07 00:11:22 +0100

    Made the decode of JSON separate from the api call code. Added live match to the api

--- components/api/Football.php
+++ components/api/Football.php
@@ -37,27 +37,33 @@ class Football extends Component
         if ($competitions && $competitions->Competition) {
             // The competitions variable is valid. Cycle through it, updating and adding new competitions.
             foreach ($competitions->Competition as $c) {
-                // Check if the competition exists already.
-                $competition = Competition::find()->where(['api_id' => $c->id])->one();
-
-                if (!$competition){
-                    $competition = new Competition;
-                    $competition->created = time();
-                }
-
-                // Now assign properties to the competition object.
-                $competition->attributes = array(
-                    'api_id' => $c->id,
-                    'name' => $c->name,
-                    'region' => $c->region,
-                    'last_updated' => time(),
-                );
+                $competition = self::getCompetition($c);
 
                 $competition->save();
             }
         } else {
             return null;
         }
+    }
+
+    private static function getCompetition($json) {
+        // Check if the competition exists already.
+        $competition = Competition::find()->where(['api_id' => $json->id])->one();
+
+        if (!$competition){
+            $competition = new Competition;
+            $competition->created = time();
+        }
+
+        // Now assign properties to the competition object.
+        $competition->attributes = array(
+            'api_id' => $json->id,
+            'name' => $json->name,
+            'region' => $json->region,
+            'last_updated' => time(),
+        );
+
+        return $competition;
     }
 
     public static function standings($competition = null) { // If it's null, get all compeition standings.
@@ -78,52 +84,58 @@ class Football extends Component
             if ($standings && $standings->teams) {
                 // The standings are valid, cycle through the teams.
                 foreach ($standings->teams as $t) {
-                    // Try and find a pre-existing team.
-                    $team = Team::find()->where(['team_id' => $t->stand_team_id])->one();
-
-                    // Check if the team exists.
-                    if (!$team) {
-                        // If not, create a new one.
-                        $team = new Team;
-                        $team->created = time();
-                    }
-
-                    // Assign all the remaining attributes.
-                    $team->attributes = array(
-                        'team_id' => $t->stand_team_id,
-                        'competition' => $competition,
-                        'name' => $t->stand_team_name,
-                        'status' => $t->stand_status,
-                        'form' => $t->stand_recent_form,
-                        'position' => $t->stand_position,
-                        'overall_gp' => $t->stand_overall_gp,
-                        'overall_w' => $t->stand_overall_w,
-                        'overall_d' => $t->stand_overall_d,
-                        'overall_l' => $t->stand_overall_l,
-                        'overall_gs' => $t->stand_overall_gs,
-                        'overall_ga' => $t->stand_overall_ga,
-                        'home_gp' => $t->stand_home_gp,
-                        'home_w' => $t->stand_home_w,
-                        'home_d' => $t->stand_home_d,
-                        'home_l' => $t->stand_home_l,
-                        'home_gs' => $t->stand_home_gs,
-                        'home_ga' => $t->stand_home_ga,
-                        'away_gp' => $t->stand_away_gp,
-                        'away_w' => $t->stand_away_w,
-                        'away_d' => $t->stand_away_d,
-                        'away_l' => $t->stand_away_l,
-                        'away_gs' => $t->stand_away_gs,
-                        'away_ga' => $t->stand_away_ga,
-                        'gd' => $t->stand_gd,
-                        'points' => $t->stand_points,
-                        'last_updated' => time(),
-                    );
+                    $team = self::getTeam($t);
 
                     // Save the team.
                     $team->save();
                 }
             }
         }
+    }
+
+    private static function getTeam($json) {
+        // Try and find a pre-existing team.
+        $team = Team::find()->where(['team_id' => $json->stand_team_id])->one();
+
+        // Check if the team exists.
+        if (!$team) {
+            // If not, create a new one.
+            $team = new Team;
+            $team->created = time();
+        }
+
+        // Assign all the remaining attributes.
+        $team->attributes = array(
+            'team_id' => $json->stand_team_id,
+            'competition' => $competition,
+            'name' => $json->stand_team_name,
+            'status' => $json->stand_status,
+            'form' => $json->stand_recent_form,
+            'position' => $json->stand_position,
+            'overall_gp' => $json->stand_overall_gp,
+            'overall_w' => $json->stand_overall_w,
+            'overall_d' => $json->stand_overall_d,
+            'overall_l' => $json->stand_overall_l,
+            'overall_gs' => $json->stand_overall_gs,
+            'overall_ga' => $json->stand_overall_ga,
+            'home_gp' => $json->stand_home_gp,
+            'home_w' => $json->stand_home_w,
+            'home_d' => $json->stand_home_d,
+            'home_l' => $json->stand_home_l,
+            'home_gs' => $json->stand_home_gs,
+            'home_ga' => $json->stand_home_ga,
+            'away_gp' => $json->stand_away_gp,
+            'away_w' => $json->stand_away_w,
+            'away_d' => $json->stand_away_d,
+            'away_l' => $json->stand_away_l,
+            'away_gs' => $json->stand_away_gs,
+            'away_ga' => $json->stand_away_ga,
+            'gd' => $json->stand_gd,
+            'points' => $json->stand_points,
+            'last_updated' => time(),
+        );
+
+        return $team;
     }
 
     public static function live($competition = null) {
@@ -140,11 +152,53 @@ class Football extends Component
         foreach ($competitions as $c) {
             $matches = self::request("today", ['comp_id' => $c->api_id]);
 
-            if ($matches) {
-                foreach ($matches as $match) {
-                    // Use http://football-api.com/documentation/ to complete this from the sample.
+            if ($matches && $matches->match) {
+                foreach ($matches as $m) {
+                    // Try and find a pre-existing match.
+                    $match = self::getMatch($m);
+                    $match->save();
                 }
             }
         }
+    }
+
+    private static function getMatch($json) {
+        // Try and find a pre-existing match.
+        $match = Match::find()->where(['match_id' => $json->match_id])->one();
+
+        // Check if the match exists.
+        if (!$match) {
+            // If not, create a new one.
+            $match = new Match;
+            $match->created = time();
+        }
+
+        // As the match may in progress, we want to know if it's completed, and if this is the first tiem we have come
+        // across the match being completed. This is to decide who wins their bets.
+        if ($match && isset($match->status) && $match->status && $match->status != "FT" && $json->status == "FT")
+            self::matchCompleted($match);
+
+        // Assign all the remaining attributes.
+        $match->attributes = array(
+            'match_id' => $json->match_id,
+            'comp_id' => $json->match_comp_id,
+            'match_date' => strtotime($json->match_formatted_date . " " . $json->match_time),
+            'status' => $json->match_status,
+            'home_team_id' => $json->match_localteam_id,
+            'home_score' => $json->match_localteam_score,
+            'away_team_id' => $json->match_visitorteam_id,
+            'away_score' => $json->match_visitorteam_score,
+            'halftime_score' => $json->match_ht_score,
+            'last_updated' => time(),
+        );
+
+        return $match;
+    }
+
+    private static function matchCompleted($match) {
+        if (!$match)
+            return false;
+
+        // TODO: Get all bets related to this match and pay them off.
     }
 }
