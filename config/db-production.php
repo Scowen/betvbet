5fbeb71dc49dfb0def54e9797f52db4b4fde8954
Xylum <lmscowen@gmail.com>
2015-10-13 01:00:23 +0100

    Production server mysql details

--- config/db-production.php
+++ config/db-production.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=betvbet',
+    'dsn' => 'mysql:host=mysql.betvbet.co.uk;dbname=betvbet',
     'username' => 'betvbet',
     'password' => 'hMTCTw5c',
     'charset' => 'utf8',
