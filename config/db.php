3c447fe801ddca067fd3f32e66fd43c79dba646d
Luke Scowen <lmscowen@gmail.com>
2015-10-16 12:46:18 +0100

    Updated DB file to be production.
    Make sure to change this to your own database credentials.

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=betvbet',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=mysql.betvbet.co.uk;dbname=betvbet',
+    'username' => 'betvbet',
+    'password' => 'hMTCTw5c',
     'charset' => 'utf8',
 ];
