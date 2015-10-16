7371c0bc6f15814d11d86dd529b9e74456f2e0fc
Xylum <lmscowen@gmail.com>
2015-10-17 00:59:30 +0100

    Modified use statements

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -6,8 +6,8 @@ use Yii;
 use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
-use app\models\LoginForm;
-use app\models\RegisterForm;
+use app\models\form\LoginForm;
+use app\models\form\RegisterForm;
 use app\components\api\Football;
 
 class SiteController extends Controller
