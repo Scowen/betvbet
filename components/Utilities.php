61c8f8dad47c37d8769dfbbfef4b16de3da4758e
Xylum <lmscowen@gmail.com>
2015-10-17 02:31:21 +0100

    Utilities class created
    Added random string generator

---
+++ components/Utilities.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\components;
+
+use Yii;
+use yii\base\Component;
+use yii\base\InvalidConfigException;
+
+class Utilities extends Component
+{
+    public static function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+}
