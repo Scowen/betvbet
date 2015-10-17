c598ae38c37945d8f6086149866c80dbff935f56
Xylum <lmscowen@gmail.com>
2015-10-17 02:31:39 +0100

    Change config for swiftmailer for future reference

--- config/web.php
+++ config/web.php
@@ -27,6 +27,16 @@ $config = [
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
             'useFileTransport' => true,
+            /*
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'username@gmail.com',
+                'password' => 'password',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
+            */
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
