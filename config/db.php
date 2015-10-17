d99af3351512c1d4d8bffd6818fd38a56918f77c
Luke Scowen <lmscowen@gmail.com>
2015-10-17 01:08:41 +0100

    Deleting db.php as deploy will automatically push this, use db-production as an example @steveoos

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhostk;dbname=betvbet',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
